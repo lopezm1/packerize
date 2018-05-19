7acb30631568091b9a42620d8e6d0e79401270a9
Miguel Lopez <MiguelA.Lopez@corp-l-7170516.adesa.com>
2018-05-18 20:04:22 -0700

    don't think i can use aws.codebuild

--- playground.py
+++ playground.py
@@ -1,5 +1,24 @@
 #GIVEN AN ARRAY OF INTERGER SLOPES, FIND THE MINIMIMUM INCREMENT/DECREMENT NEEDED TO MAKE THE ARRAY INCREASING OR DECREASING
-#9,7,8,6,2,3,3 -> 2
-#1,2,2,3,3,4 ->0 (ALREADY INCREASING
+#[9,7,8,6,2,3,3] -> 2
+#[1,2,2,3,3,4] ->0 (ALREADY INCREASING
 
-def minChange(slopes):
+def minChange(_slopes):
+    # if negative, we are decreasing
+    # if positive, we are increasing
+    direction = _slopes[-1] - _slopes[0]
+
+    # total change
+    min = 0
+
+    for i in range:
+        print(i,fibonacci[i])
+
+
+
+
+
+
+if __name__ == '__main__':
+    slope1 = [9,7,8,6,2,3,3]
+    slope2 = [1,2,2,3,3,4]
+    minChange()
